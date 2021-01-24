1d3bec50d70244495161c320f5c3f81f60455ff3
coalpha <22552895+coalpha@users.noreply.github.com>
2021-01-24 02:28:55 -0800

    looks good

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # linguist-colors
 
+![](misc/icon.png)
+
 They disabled github pages on linguist.
 See [linguist/pull/5029](https://github.com/github/linguist/pull/5029#issuecomment-759345026).
 
--- main.js
+++ main.js
@@ -1,11 +1,45 @@
 const d = document;
 
-const lang_div = d.getElementById("languages");
+const lang_req = new XMLHttpRequest();
+const yamlfile = "https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml"
+lang_req.open("GET", yamlfile, false);
+lang_req.send(null);
 
-const lang_prom = fetch("https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml").then(r => r.text());
+if (lang_req.status !== 200) {
+   throw request.status;
+}
 
-void async function main() {
-   const langs = jsyaml.load(await lang_prom);
-   console.log(langs);
-   debugger;
-}();
+class LangInfo extends HTMLElement {
+   constructor(name, hex) {
+      super();
+      if (hex == null) {
+         this.style.display = "none";
+         return;
+      }
+
+      const title = d.createElement("span");
+      title.innerText = name;
+
+      const color = d.createElement("div");
+      color.classList.add("color");
+      color.style.backgroundColor = hex;
+
+      this.appendChild(title);
+      this.appendChild(color);
+   }
+}
+
+customElements.define("lang-info", LangInfo);
+
+const languages = jsyaml.load(lang_req.responseText);
+const lang_names = Object.keys(languages);
+const lang_colors = Object.fromEntries(
+   Object.entries(languages)
+      .map(([name, {color}]) => [name, color])
+);
+
+lang_names.sort();
+
+lang_names.forEach(
+   name => d.body.appendChild(new LangInfo(name, lang_colors[name]))
+);
